b1399fbab6faee05e1275b5fcfb90c64f8a8b9cb
Raymon Lacy <raymon@example.com>
2025-05-27 13:16:33 -0700

    chore: add babel-jest + presets to transform JSX tests

---
+++ dashboard_v3/jest.config.js
@@ -0,0 +1,9 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: { '\\.(css|less)$': 'identity-obj-proxy' },
+  setupFilesAfterEnv: ['@testing-library/jest-dom']
+};
