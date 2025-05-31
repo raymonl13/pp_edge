4d0b3bf2ab1717d9c3456543b21a9080f6f6627f
Raymon Lacy <raymon@example.com>
2025-05-31 08:58:00 -0700

    chore: restore model_assets and local tweaks

--- dashboard_v3/dashboard_v3/jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  testEnvironment: 'jsdom',
-  transform: { '^.+\\.jsx?$': 'babel-jest' },
-  transformIgnorePatterns: ['/node_modules/'],
-  moduleNameMapper: { '\\.(css|less)$': 'identity-obj-proxy' },
-  setupFilesAfterEnv: ['@testing-library/jest-dom', './jest.setup.js']
-};
--- dashboard_v3/dashboard_v3/jest.setup.js
+++
@@ -1,4 +0,0 @@
-global.fetch = async () => ({
-  text: async () =>
-    'player,edge,spark\nA,5,"[1,2]"\nB,7,"[3,4]"',
-});
